7d120ca276d231f898ef97818ab4fe421540ff10
streammachineio-org <apis@streammachine.io>
2021-08-20 13:24:14 +0000

    Snapshot 1.18.0-aa167d80

--- api/metrics/v1/metrics_grpc.pb.go
+++ api/metrics/v1/metrics_grpc.pb.go
@@ -19,7 +19,6 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type MetricsServiceClient interface {
 	GetSystemState(ctx context.Context, in *GetSystemStateRequest, opts ...grpc.CallOption) (*GetSystemStateResponse, error)
-	GetLatency(ctx context.Context, in *GetLatencyRequest, opts ...grpc.CallOption) (*GetLatencyResponse, error)
 }
 
 type metricsServiceClient struct {
@@ -39,21 +38,11 @@ func (c *metricsServiceClient) GetSystemState(ctx context.Context, in *GetSystem
 	return out, nil
 }
 
-func (c *metricsServiceClient) GetLatency(ctx context.Context, in *GetLatencyRequest, opts ...grpc.CallOption) (*GetLatencyResponse, error) {
-	out := new(GetLatencyResponse)
-	err := c.cc.Invoke(ctx, "/streammachine.api.metrics.v1.MetricsService/GetLatency", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // MetricsServiceServer is the server API for MetricsService service.
 // All implementations must embed UnimplementedMetricsServiceServer
 // for forward compatibility
 type MetricsServiceServer interface {
 	GetSystemState(context.Context, *GetSystemStateRequest) (*GetSystemStateResponse, error)
-	GetLatency(context.Context, *GetLatencyRequest) (*GetLatencyResponse, error)
 	mustEmbedUnimplementedMetricsServiceServer()
 }
 
@@ -63,9 +52,6 @@ type UnimplementedMetricsServiceServer struct {
 
 func (UnimplementedMetricsServiceServer) GetSystemState(context.Context, *GetSystemStateRequest) (*GetSystemStateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetSystemState not implemented")
-}
-func (UnimplementedMetricsServiceServer) GetLatency(context.Context, *GetLatencyRequest) (*GetLatencyResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetLatency not implemented")
 }
 func (UnimplementedMetricsServiceServer) mustEmbedUnimplementedMetricsServiceServer() {}
 
@@ -98,24 +84,6 @@ func _MetricsService_GetSystemState_Handler(srv interface{}, ctx context.Context
 	return interceptor(ctx, in, info, handler)
 }
 
-func _MetricsService_GetLatency_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetLatencyRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MetricsServiceServer).GetLatency(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/streammachine.api.metrics.v1.MetricsService/GetLatency",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MetricsServiceServer).GetLatency(ctx, req.(*GetLatencyRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 // MetricsService_ServiceDesc is the grpc.ServiceDesc for MetricsService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -126,10 +94,6 @@ var MetricsService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetSystemState",
 			Handler:    _MetricsService_GetSystemState_Handler,
-		},
-		{
-			MethodName: "GetLatency",
-			Handler:    _MetricsService_GetLatency_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
