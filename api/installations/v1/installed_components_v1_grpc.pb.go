ee6f1dc7b17914b6c4530d661e37f21a5ee58da5
strmprivacy-io <apis@strmprivacy.io>
2022-04-29 07:46:18 +0000

    Snapshot 2.31.0-52a677a8

--- api/installations/v1/installed_components_v1_grpc.pb.go
+++ api/installations/v1/installed_components_v1_grpc.pb.go
@@ -30,7 +30,7 @@ type InstalledComponentsServiceClient interface {
 	ListInstalledComponents(ctx context.Context, in *ListInstalledComponentsRequest, opts ...grpc.CallOption) (*ListInstalledComponentsResponse, error)
 	// Authentication goes through the users realm; a strm-external-user-id is required in the metadata. Similar to list,
 	// but only lists the state of the instances in the last x minutes
-	ListInstalledComponentsActualStates(ctx context.Context, in *ListInstalledComponentsActualStatesRequest, opts ...grpc.CallOption) (*ListInstalledComponentsActualStatesResponse, error)
+	ListInstalledComponentsCurrentStates(ctx context.Context, in *ListInstalledComponentsCurrentStatesRequest, opts ...grpc.CallOption) (*ListInstalledComponentsCurrentStatesResponse, error)
 }
 
 type installedComponentsServiceClient struct {
@@ -68,9 +68,9 @@ func (c *installedComponentsServiceClient) ListInstalledComponents(ctx context.C
 	return out, nil
 }
 
-func (c *installedComponentsServiceClient) ListInstalledComponentsActualStates(ctx context.Context, in *ListInstalledComponentsActualStatesRequest, opts ...grpc.CallOption) (*ListInstalledComponentsActualStatesResponse, error) {
-	out := new(ListInstalledComponentsActualStatesResponse)
-	err := c.cc.Invoke(ctx, "/strmprivacy.api.installations.v1.InstalledComponentsService/ListInstalledComponentsActualStates", in, out, opts...)
+func (c *installedComponentsServiceClient) ListInstalledComponentsCurrentStates(ctx context.Context, in *ListInstalledComponentsCurrentStatesRequest, opts ...grpc.CallOption) (*ListInstalledComponentsCurrentStatesResponse, error) {
+	out := new(ListInstalledComponentsCurrentStatesResponse)
+	err := c.cc.Invoke(ctx, "/strmprivacy.api.installations.v1.InstalledComponentsService/ListInstalledComponentsCurrentStates", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -89,7 +89,7 @@ type InstalledComponentsServiceServer interface {
 	ListInstalledComponents(context.Context, *ListInstalledComponentsRequest) (*ListInstalledComponentsResponse, error)
 	// Authentication goes through the users realm; a strm-external-user-id is required in the metadata. Similar to list,
 	// but only lists the state of the instances in the last x minutes
-	ListInstalledComponentsActualStates(context.Context, *ListInstalledComponentsActualStatesRequest) (*ListInstalledComponentsActualStatesResponse, error)
+	ListInstalledComponentsCurrentStates(context.Context, *ListInstalledComponentsCurrentStatesRequest) (*ListInstalledComponentsCurrentStatesResponse, error)
 	mustEmbedUnimplementedInstalledComponentsServiceServer()
 }
 
@@ -106,8 +106,8 @@ func (UnimplementedInstalledComponentsServiceServer) GetInstalledComponent(conte
 func (UnimplementedInstalledComponentsServiceServer) ListInstalledComponents(context.Context, *ListInstalledComponentsRequest) (*ListInstalledComponentsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListInstalledComponents not implemented")
 }
-func (UnimplementedInstalledComponentsServiceServer) ListInstalledComponentsActualStates(context.Context, *ListInstalledComponentsActualStatesRequest) (*ListInstalledComponentsActualStatesResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListInstalledComponentsActualStates not implemented")
+func (UnimplementedInstalledComponentsServiceServer) ListInstalledComponentsCurrentStates(context.Context, *ListInstalledComponentsCurrentStatesRequest) (*ListInstalledComponentsCurrentStatesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListInstalledComponentsCurrentStates not implemented")
 }
 func (UnimplementedInstalledComponentsServiceServer) mustEmbedUnimplementedInstalledComponentsServiceServer() {
 }
@@ -177,20 +177,20 @@ func _InstalledComponentsService_ListInstalledComponents_Handler(srv interface{}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _InstalledComponentsService_ListInstalledComponentsActualStates_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ListInstalledComponentsActualStatesRequest)
+func _InstalledComponentsService_ListInstalledComponentsCurrentStates_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListInstalledComponentsCurrentStatesRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(InstalledComponentsServiceServer).ListInstalledComponentsActualStates(ctx, in)
+		return srv.(InstalledComponentsServiceServer).ListInstalledComponentsCurrentStates(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/strmprivacy.api.installations.v1.InstalledComponentsService/ListInstalledComponentsActualStates",
+		FullMethod: "/strmprivacy.api.installations.v1.InstalledComponentsService/ListInstalledComponentsCurrentStates",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(InstalledComponentsServiceServer).ListInstalledComponentsActualStates(ctx, req.(*ListInstalledComponentsActualStatesRequest))
+		return srv.(InstalledComponentsServiceServer).ListInstalledComponentsCurrentStates(ctx, req.(*ListInstalledComponentsCurrentStatesRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -215,8 +215,8 @@ var InstalledComponentsService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _InstalledComponentsService_ListInstalledComponents_Handler,
 		},
 		{
-			MethodName: "ListInstalledComponentsActualStates",
-			Handler:    _InstalledComponentsService_ListInstalledComponentsActualStates_Handler,
+			MethodName: "ListInstalledComponentsCurrentStates",
+			Handler:    _InstalledComponentsService_ListInstalledComponentsCurrentStates_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
