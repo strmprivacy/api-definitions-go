0d1d1ce378827608e8c8776ea29ccc21b188ad68
strmprivacy-io <apis@strmprivacy.io>
2023-09-05 07:30:59 +0000

    Snapshot 3.16.0-29ecc5e3

--- api/paging/v1/paging_v1.pb.go
+++ api/paging/v1/paging_v1.pb.go
@@ -20,6 +20,55 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+type FieldsOffsetPageToken_Ordering int32
+
+const (
+	FieldsOffsetPageToken_ORDERING_UNSPECIFIED FieldsOffsetPageToken_Ordering = 0
+	FieldsOffsetPageToken_ASC                  FieldsOffsetPageToken_Ordering = 1
+	FieldsOffsetPageToken_DESC                 FieldsOffsetPageToken_Ordering = 2
+)
+
+// Enum value maps for FieldsOffsetPageToken_Ordering.
+var (
+	FieldsOffsetPageToken_Ordering_name = map[int32]string{
+		0: "ORDERING_UNSPECIFIED",
+		1: "ASC",
+		2: "DESC",
+	}
+	FieldsOffsetPageToken_Ordering_value = map[string]int32{
+		"ORDERING_UNSPECIFIED": 0,
+		"ASC":                  1,
+		"DESC":                 2,
+	}
+)
+
+func (x FieldsOffsetPageToken_Ordering) Enum() *FieldsOffsetPageToken_Ordering {
+	p := new(FieldsOffsetPageToken_Ordering)
+	*p = x
+	return p
+}
+
+func (x FieldsOffsetPageToken_Ordering) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (FieldsOffsetPageToken_Ordering) Descriptor() protoreflect.EnumDescriptor {
+	return file_strmprivacy_api_paging_v1_paging_v1_proto_enumTypes[0].Descriptor()
+}
+
+func (FieldsOffsetPageToken_Ordering) Type() protoreflect.EnumType {
+	return &file_strmprivacy_api_paging_v1_paging_v1_proto_enumTypes[0]
+}
+
+func (x FieldsOffsetPageToken_Ordering) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use FieldsOffsetPageToken_Ordering.Descriptor instead.
+func (FieldsOffsetPageToken_Ordering) EnumDescriptor() ([]byte, []int) {
+	return file_strmprivacy_api_paging_v1_paging_v1_proto_rawDescGZIP(), []int{2, 0}
+}
+
 type TimestampPageToken struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -70,6 +119,159 @@ func (x *TimestampPageToken) GetTimestamp() string {
 	return ""
 }
 
+type OffsetPageToken struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Offset int64 `protobuf:"varint,1,opt,name=offset,proto3" json:"offset,omitempty"`
+}
+
+func (x *OffsetPageToken) Reset() {
+	*x = OffsetPageToken{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *OffsetPageToken) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*OffsetPageToken) ProtoMessage() {}
+
+func (x *OffsetPageToken) ProtoReflect() protoreflect.Message {
+	mi := &file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use OffsetPageToken.ProtoReflect.Descriptor instead.
+func (*OffsetPageToken) Descriptor() ([]byte, []int) {
+	return file_strmprivacy_api_paging_v1_paging_v1_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *OffsetPageToken) GetOffset() int64 {
+	if x != nil {
+		return x.Offset
+	}
+	return 0
+}
+
+// A page token that contains all fields that were used for ordering results.
+// Each field contains the last known value and the ordering direction, which
+// allows to use this in combination with the seek operator in SQL databases.
+type FieldsOffsetPageToken struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// key = field name, value = field offset information
+	Fields map[string]*FieldsOffsetPageToken_FieldOffset `protobuf:"bytes,1,rep,name=fields,proto3" json:"fields,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+}
+
+func (x *FieldsOffsetPageToken) Reset() {
+	*x = FieldsOffsetPageToken{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *FieldsOffsetPageToken) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FieldsOffsetPageToken) ProtoMessage() {}
+
+func (x *FieldsOffsetPageToken) ProtoReflect() protoreflect.Message {
+	mi := &file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FieldsOffsetPageToken.ProtoReflect.Descriptor instead.
+func (*FieldsOffsetPageToken) Descriptor() ([]byte, []int) {
+	return file_strmprivacy_api_paging_v1_paging_v1_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *FieldsOffsetPageToken) GetFields() map[string]*FieldsOffsetPageToken_FieldOffset {
+	if x != nil {
+		return x.Fields
+	}
+	return nil
+}
+
+type FieldsOffsetPageToken_FieldOffset struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	LastValue string                         `protobuf:"bytes,1,opt,name=last_value,json=lastValue,proto3" json:"last_value,omitempty"`
+	Ordering  FieldsOffsetPageToken_Ordering `protobuf:"varint,2,opt,name=ordering,proto3,enum=strmprivacy.api.paging.v1.FieldsOffsetPageToken_Ordering" json:"ordering,omitempty"`
+}
+
+func (x *FieldsOffsetPageToken_FieldOffset) Reset() {
+	*x = FieldsOffsetPageToken_FieldOffset{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *FieldsOffsetPageToken_FieldOffset) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FieldsOffsetPageToken_FieldOffset) ProtoMessage() {}
+
+func (x *FieldsOffsetPageToken_FieldOffset) ProtoReflect() protoreflect.Message {
+	mi := &file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FieldsOffsetPageToken_FieldOffset.ProtoReflect.Descriptor instead.
+func (*FieldsOffsetPageToken_FieldOffset) Descriptor() ([]byte, []int) {
+	return file_strmprivacy_api_paging_v1_paging_v1_proto_rawDescGZIP(), []int{2, 1}
+}
+
+func (x *FieldsOffsetPageToken_FieldOffset) GetLastValue() string {
+	if x != nil {
+		return x.LastValue
+	}
+	return ""
+}
+
+func (x *FieldsOffsetPageToken_FieldOffset) GetOrdering() FieldsOffsetPageToken_Ordering {
+	if x != nil {
+		return x.Ordering
+	}
+	return FieldsOffsetPageToken_ORDERING_UNSPECIFIED
+}
+
 var File_strmprivacy_api_paging_v1_paging_v1_proto protoreflect.FileDescriptor
 
 var file_strmprivacy_api_paging_v1_paging_v1_proto_rawDesc = []byte{
@@ -80,14 +282,43 @@ var file_strmprivacy_api_paging_v1_paging_v1_proto_rawDesc = []byte{
 	0x69, 0x6e, 0x67, 0x2e, 0x76, 0x31, 0x22, 0x32, 0x0a, 0x12, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74,
 	0x61, 0x6d, 0x70, 0x50, 0x61, 0x67, 0x65, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x12, 0x1c, 0x0a, 0x09,
 	0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x42, 0x63, 0x0a, 0x1c, 0x69, 0x6f,
-	0x2e, 0x73, 0x74, 0x72, 0x6d, 0x70, 0x72, 0x69, 0x76, 0x61, 0x63, 0x79, 0x2e, 0x61, 0x70, 0x69,
-	0x2e, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x2e, 0x76, 0x31, 0x50, 0x01, 0x5a, 0x41, 0x67, 0x69,
-	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x74, 0x72, 0x6d, 0x70, 0x72, 0x69,
-	0x76, 0x61, 0x63, 0x79, 0x2f, 0x61, 0x70, 0x69, 0x2d, 0x64, 0x65, 0x66, 0x69, 0x6e, 0x69, 0x74,
-	0x69, 0x6f, 0x6e, 0x73, 0x2d, 0x67, 0x6f, 0x2f, 0x76, 0x33, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x70,
-	0x61, 0x67, 0x69, 0x6e, 0x67, 0x2f, 0x76, 0x31, 0x3b, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x22, 0x29, 0x0a, 0x0f, 0x4f, 0x66,
+	0x66, 0x73, 0x65, 0x74, 0x50, 0x61, 0x67, 0x65, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x12, 0x16, 0x0a,
+	0x06, 0x6f, 0x66, 0x66, 0x73, 0x65, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x6f,
+	0x66, 0x66, 0x73, 0x65, 0x74, 0x22, 0xa5, 0x03, 0x0a, 0x15, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x73,
+	0x4f, 0x66, 0x66, 0x73, 0x65, 0x74, 0x50, 0x61, 0x67, 0x65, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x12,
+	0x54, 0x0a, 0x06, 0x66, 0x69, 0x65, 0x6c, 0x64, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32,
+	0x3c, 0x2e, 0x73, 0x74, 0x72, 0x6d, 0x70, 0x72, 0x69, 0x76, 0x61, 0x63, 0x79, 0x2e, 0x61, 0x70,
+	0x69, 0x2e, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x2e, 0x76, 0x31, 0x2e, 0x46, 0x69, 0x65, 0x6c,
+	0x64, 0x73, 0x4f, 0x66, 0x66, 0x73, 0x65, 0x74, 0x50, 0x61, 0x67, 0x65, 0x54, 0x6f, 0x6b, 0x65,
+	0x6e, 0x2e, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x73, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x52, 0x06, 0x66,
+	0x69, 0x65, 0x6c, 0x64, 0x73, 0x1a, 0x77, 0x0a, 0x0b, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x73, 0x45,
+	0x6e, 0x74, 0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x52, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x3c, 0x2e, 0x73, 0x74, 0x72, 0x6d, 0x70, 0x72, 0x69, 0x76,
+	0x61, 0x63, 0x79, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x2e, 0x76,
+	0x31, 0x2e, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x73, 0x4f, 0x66, 0x66, 0x73, 0x65, 0x74, 0x50, 0x61,
+	0x67, 0x65, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x2e, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x4f, 0x66, 0x66,
+	0x73, 0x65, 0x74, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x1a, 0x83,
+	0x01, 0x0a, 0x0b, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x4f, 0x66, 0x66, 0x73, 0x65, 0x74, 0x12, 0x1d,
+	0x0a, 0x0a, 0x6c, 0x61, 0x73, 0x74, 0x5f, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x09, 0x6c, 0x61, 0x73, 0x74, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x12, 0x55, 0x0a,
+	0x08, 0x6f, 0x72, 0x64, 0x65, 0x72, 0x69, 0x6e, 0x67, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0e, 0x32,
+	0x39, 0x2e, 0x73, 0x74, 0x72, 0x6d, 0x70, 0x72, 0x69, 0x76, 0x61, 0x63, 0x79, 0x2e, 0x61, 0x70,
+	0x69, 0x2e, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x2e, 0x76, 0x31, 0x2e, 0x46, 0x69, 0x65, 0x6c,
+	0x64, 0x73, 0x4f, 0x66, 0x66, 0x73, 0x65, 0x74, 0x50, 0x61, 0x67, 0x65, 0x54, 0x6f, 0x6b, 0x65,
+	0x6e, 0x2e, 0x4f, 0x72, 0x64, 0x65, 0x72, 0x69, 0x6e, 0x67, 0x52, 0x08, 0x6f, 0x72, 0x64, 0x65,
+	0x72, 0x69, 0x6e, 0x67, 0x22, 0x37, 0x0a, 0x08, 0x4f, 0x72, 0x64, 0x65, 0x72, 0x69, 0x6e, 0x67,
+	0x12, 0x18, 0x0a, 0x14, 0x4f, 0x52, 0x44, 0x45, 0x52, 0x49, 0x4e, 0x47, 0x5f, 0x55, 0x4e, 0x53,
+	0x50, 0x45, 0x43, 0x49, 0x46, 0x49, 0x45, 0x44, 0x10, 0x00, 0x12, 0x07, 0x0a, 0x03, 0x41, 0x53,
+	0x43, 0x10, 0x01, 0x12, 0x08, 0x0a, 0x04, 0x44, 0x45, 0x53, 0x43, 0x10, 0x02, 0x42, 0x63, 0x0a,
+	0x1c, 0x69, 0x6f, 0x2e, 0x73, 0x74, 0x72, 0x6d, 0x70, 0x72, 0x69, 0x76, 0x61, 0x63, 0x79, 0x2e,
+	0x61, 0x70, 0x69, 0x2e, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x2e, 0x76, 0x31, 0x50, 0x01, 0x5a,
+	0x41, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x74, 0x72, 0x6d,
+	0x70, 0x72, 0x69, 0x76, 0x61, 0x63, 0x79, 0x2f, 0x61, 0x70, 0x69, 0x2d, 0x64, 0x65, 0x66, 0x69,
+	0x6e, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x2d, 0x67, 0x6f, 0x2f, 0x76, 0x33, 0x2f, 0x61, 0x70,
+	0x69, 0x2f, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x67, 0x2f, 0x76, 0x31, 0x3b, 0x70, 0x61, 0x67, 0x69,
+	0x6e, 0x67, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -102,16 +333,25 @@ func file_strmprivacy_api_paging_v1_paging_v1_proto_rawDescGZIP() []byte {
 	return file_strmprivacy_api_paging_v1_paging_v1_proto_rawDescData
 }
 
-var file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_strmprivacy_api_paging_v1_paging_v1_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_strmprivacy_api_paging_v1_paging_v1_proto_goTypes = []interface{}{
-	(*TimestampPageToken)(nil), // 0: strmprivacy.api.paging.v1.TimestampPageToken
+	(FieldsOffsetPageToken_Ordering)(0),       // 0: strmprivacy.api.paging.v1.FieldsOffsetPageToken.Ordering
+	(*TimestampPageToken)(nil),                // 1: strmprivacy.api.paging.v1.TimestampPageToken
+	(*OffsetPageToken)(nil),                   // 2: strmprivacy.api.paging.v1.OffsetPageToken
+	(*FieldsOffsetPageToken)(nil),             // 3: strmprivacy.api.paging.v1.FieldsOffsetPageToken
+	nil,                                       // 4: strmprivacy.api.paging.v1.FieldsOffsetPageToken.FieldsEntry
+	(*FieldsOffsetPageToken_FieldOffset)(nil), // 5: strmprivacy.api.paging.v1.FieldsOffsetPageToken.FieldOffset
 }
 var file_strmprivacy_api_paging_v1_paging_v1_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	4, // 0: strmprivacy.api.paging.v1.FieldsOffsetPageToken.fields:type_name -> strmprivacy.api.paging.v1.FieldsOffsetPageToken.FieldsEntry
+	5, // 1: strmprivacy.api.paging.v1.FieldsOffsetPageToken.FieldsEntry.value:type_name -> strmprivacy.api.paging.v1.FieldsOffsetPageToken.FieldOffset
+	0, // 2: strmprivacy.api.paging.v1.FieldsOffsetPageToken.FieldOffset.ordering:type_name -> strmprivacy.api.paging.v1.FieldsOffsetPageToken.Ordering
+	3, // [3:3] is the sub-list for method output_type
+	3, // [3:3] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_strmprivacy_api_paging_v1_paging_v1_proto_init() }
@@ -132,19 +372,56 @@ func file_strmprivacy_api_paging_v1_paging_v1_proto_init() {
 				return nil
 			}
 		}
+		file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*OffsetPageToken); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*FieldsOffsetPageToken); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*FieldsOffsetPageToken_FieldOffset); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_strmprivacy_api_paging_v1_paging_v1_proto_rawDesc,
-			NumEnums:      0,
-			NumMessages:   1,
+			NumEnums:      1,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_strmprivacy_api_paging_v1_paging_v1_proto_goTypes,
 		DependencyIndexes: file_strmprivacy_api_paging_v1_paging_v1_proto_depIdxs,
+		EnumInfos:         file_strmprivacy_api_paging_v1_paging_v1_proto_enumTypes,
 		MessageInfos:      file_strmprivacy_api_paging_v1_paging_v1_proto_msgTypes,
 	}.Build()
 	File_strmprivacy_api_paging_v1_paging_v1_proto = out.File
